4295f49c86a4140717e758ea4862966236766b02
Simon Brown <simonandon@live.com.au>
2024-05-26 10:56:05 +0800

    Created Comment model

---
+++ models/Comment.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Comment inherits from Model
+class Comment extends Model {
+	// instance method to increment likes
+	incLikes() {
+		this.likes++;
+	};
+	// instance method to decrement likes
+	decLikes() {
+		this.likes--;
+	};
+}
+
+// initialise Comment model
+Comment.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		content: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		date_created: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.NOW,
+		},
+		likes: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 0,
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'user',
+				key: 'id',
+			},
+		},
+		post_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'post',
+				key: 'id',
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'comment',
+	}
+);
+
+module.exports = Comment;
