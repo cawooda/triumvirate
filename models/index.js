049c5afd96d2a50e7bab1974f927340cf4e99984
Simon Brown <simonandon@live.com.au>
2024-05-27 09:31:21 +0800

    Set up relationships between User and Post, User and Comment, and Post and Comment

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// one-to-many relationship between User and Post
+User.hasMany(Post, {
+	foreignKey: 'user_id',
+	onDelete: 'CASCADE',
+});
+Post.belongsTo(User, {
+	foreignKey: 'user_id',
+});
+
+// one-to-many relationship between User and Comment
+User.hasMany(Comment, {
+	foreignKey: 'user_id',
+	onDelete: 'CASCADE',
+});
+Comment.belongsTo(User, {
+	foreignKey: 'user_id',
+});
+
+// one-to-many relationship between Post and Comment
+Post.hasMany(Comment, {
+	foreignKey: 'post_id',
+	onDelete: 'CASCADE',
+});
+Comment.belongsTo(Post, {
+	foreignKey: 'post_id',
+});
