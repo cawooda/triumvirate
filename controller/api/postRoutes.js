b3e5f28b525f4d173784e6fb57452091f21b01af
Andrew Cawood <cawooda@gmail.com>
2024-05-27 21:11:16 +1000

    working controller serving api posts

--- controller/api/postRoutes.js
+++ controller/api/postRoutes.js
@@ -1,28 +1,34 @@
 const router = require('express').Router();
-const Post = require('Post');
+const {Post,Comment,User} = require('../../models');
 
+const modelName = 'Post';
 
 router.get('/', async (req,res) => {
-    const body = req.body;
-    
-    const postData = await Post.findAll();
-    
-    const response = {
-        id:'1',
-        testResponse: true
-    };
-
     try {
+        const postData = await Post.findAll({
+            include: [Comment,User]
+        });
         console.log(`Get Requesting all ${modelName}`);
+        res.status(200).json(postData);
     } catch (error) {
         console.log(`ERROR: Get Requesting all ${modelName} and we've sent back the body you sent us`)
         res.status(400).json(body);
     }
-    res.status(200).json(response);
+    
 });
 
-router.get('/getSomething', async (req,res) => {
-    
+router.get('/:id', async (req,res) => {
+    const id = req.params.id;
+    try {
+        const post = await Post.findByPk(id,{
+            include: Comment
+        });
+        console.log(`Get Requesting all ${modelName} by id ${id}`);
+        res.status(200).json(post);
+    } catch (error) {
+        console.log(`ERROR: Get Requesting all ${modelName} and we've sent back the body you sent us`)
+        res.status(400).json(body);
+    }
 });
 
 router.put('/putSomething', async (req,res) => {
