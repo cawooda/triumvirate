cb6cf9dacb6ba374a547decb4283784f75c10981
Simon Brown <simonandon@live.com.au>
2024-05-27 10:35:26 +0800

    Created Chat model

---
+++ models/Chat.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Chat inherits from Model
+class Chat extends Model {}
+
+// initialise Chat model
+Chat.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		user_a: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: 'user',
+				key: 'id'
+			},
+		},
+		user_b: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			references: {
+				model: 'user',
+				key: 'id'
+			},
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'media',
+	},
+);
+
+module.exports = Chat;
