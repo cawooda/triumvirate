a9f11b7160b253fb5f7d8b9f652e351491925688
Simon Brown <simonandon@live.com.au>
2024-05-27 09:51:25 +0800

    Created Media model

---
+++ models/Media.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Media inherits from Model
+class Media extends Model {}
+
+// initialise Media model
+Media.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		filename: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		original_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		mimetype: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		path: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		size: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'media',
+	}
+);
+
+module.exports = Media;
