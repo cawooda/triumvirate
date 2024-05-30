d8a8140ad138c61a4e3a915c49416dae49f8014d
Simon Brown <simonandon@live.com.au>
2024-05-30 12:00:41 +0800

    Imported isLoggedIn middleware and created /create-post route to render create-post template

--- controller/homeRoutes/postRoutes.js
+++ controller/homeRoutes/postRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../../models');
 
+// import custom middleware
+const isLoggedIn = require('../../utils/isLoggedIn');
+
 const modelName = 'Post';
 
 router.get('/', async (req, res) => {
@@ -14,6 +17,18 @@ router.get('/', async (req, res) => {
 	console.log('posts', posts);
 	//res.send('route home reached');
 	res.render('posts', { posts, logged_in: req.session.logged_in });
+});
+
+// route to get add new post form
+router.get('/create-post', isLoggedIn, async (req, res) => {
+	try {
+		// render create-post page
+		res.status(200).render('create-post', {
+			loggedIn: req.session.logged_in,
+		});
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 module.exports = router;
