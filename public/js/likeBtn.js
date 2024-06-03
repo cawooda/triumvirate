6c84eac053e561a1072872dee6fd1964dc410a6f
Simon Brown <simonandon@live.com.au>
2024-06-03 13:38:22 +0800

    Now checking if user is logged in before incrementing likes

--- public/js/likeBtn.js
+++ public/js/likeBtn.js
@@ -3,21 +3,29 @@ const likeBtnHandler = async (event) => {
 	// prevent default form behaviour
 	event.preventDefault();
 
-	// extract post id from dataset
-	const postId = document.querySelector('#like-btn').dataset.postId;
+	// extract logged_in from dataset
+	const loggedIn = document.querySelector('#like-btn').dataset.loggedIn;
 
-	console.log(postId);
-
-	// api request to increment likes
-	const response = await fetch(`/api/posts/like/${postId}`, {
-		method: 'PUT',
-	});
-
-	// if request successful reload page
-	if (response.ok) {
-		document.location.reload();
+	// if user not logged in alert they must login to like
+	if (!loggedIn) {
+		alert('Must be logged in to like posts.');
 	} else {
-		alert('Failed to like post.');
+		// extract post id from dataset
+		const postId = document.querySelector('#like-btn').dataset.postId;
+
+		console.log(postId);
+
+		// api request to increment likes
+		const response = await fetch(`/api/posts/like/${postId}`, {
+			method: 'PUT',
+		});
+
+		// if request successful reload page
+		if (response.ok) {
+			document.location.reload();
+		} else {
+			alert('Failed to like post.');
+		}
 	}
 };
 
